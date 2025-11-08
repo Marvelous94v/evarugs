9682dd99569bb706c53916f6ae2b54ed65024fa9
Marvelous94v <39409667+Marvelous94v@users.noreply.github.com>
2025-11-08 13:06:50 +0300

    Lesson 2.1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,43 @@ const buttonModals = document.querySelectorAll('.modal__button');
 const modalClose = document.querySelector('.modal_close');
 const body = document.querySelector('body');
 
+const contents = document.querySelectorAll('.program-line__content');
+
+
+contents.forEach((e) => {
+
+    const title = e.querySelector('.program-line__title');
+    const descr = e.querySelector('.program-line__descr');
+
+    /*     
+        // ver 1 (записывается в свойство, только одна значение свойства = 1 функция для выполнения)
+
+        console.dir(title);
+    
+        title.onclick = () => {
+            console.log(title);
+        } 
+       
+        // ver 2 (можно подключать сколько угодно событий на один элемент )
+        title.addEventListener('click', () => {
+            console.log('test');
+        })
+
+    */
+
+    title.addEventListener('click', () => {
+        descr.classList.toggle('active')
+        // console.log(title);
+    })
+
+    // console.dir(descr.classList.remove);
+
+})
+
+
+
+
+
 
 buttonModals.forEach((item) => {
     item.addEventListener('click', () => {
@@ -25,3 +62,28 @@ modalClose.addEventListener('click', () => {
     modalWindow.style.display = 'none';
     body.classList.remove('noneScroll');
 })
+
+
+
+
+/* 
+const element = document.querySelector('.header');
+const elements = document.querySelectorAll('div')
+
+console.dir(elements);
+
+for (let i = 0; i < elements.length; i++) {
+
+    console.log(elements[i]);
+
+}
+
+elements.forEach((e, i, a) => {
+    console.log(e);
+    console.log(i);
+    console.log(a);
+
+})
+
+ */
+
